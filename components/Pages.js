1c6fb33b9e9448375aec5e0e9a29da0ec94a5246
aldipee <aldipeee@gmail.com>
2020-05-25 11:52:58 +0700

    change background color

--- components/Pages.js
+++ components/Pages.js
@@ -8,7 +8,7 @@ const theme = {
   black: '#393939',
   grey: '#3a3a3a',
   lightgrey: '#e1e1e1',
-  offWhite: '#EDEDED',
+  offWhite: '#FFF',
   maxWidth: '1000px',
   bs: '0 12px 24px 0 rgba(0,0,0,0.09)',
 }
